bd469c6a7166dcfd9f7c16b4fccf0af9a035bc6e
ganeshnbadgujar <ganeshnbadgujar@gmail.com>
2018-07-01 00:21:54 +0530

    UnitTestProject

---
+++ GfK_HRFeedback/UnitTestProject1/HRFeedbackDataTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using GfK_HRFeedback.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GfK_HRFeedback.Test
+{
+    [TestClass]
+    public class HRFeedbackDataTest
+    {
+        [TestMethod]
+        public void GetQuestions()
+        {
+            QuestionsMock mock = new QuestionsMock();
+
+            List<Questions> result = mock.GetQuestionsAndAnswers();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(3, result[0].Answers.Count);
+            Assert.AreEqual("50%", result[0].Answers[0].Result);
+
+        }
+    }
+}
---
+++ GfK_HRFeedback/UnitTestProject1/QuestionsMock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using GfK_HRFeedback.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GfK_HRFeedback.Test
+{
+    public class QuestionsMock
+    {
+        public List<Questions> GetQuestionsAndAnswers()
+        {
+            List<Questions> _questions = new List<Questions>();
+
+            List<Answers> ansList = new List<Answers>();
+            ansList.Add(new Answers { ID = 1, QuestionID = 1, Text = "StackOverflow", Counter = 1, Result = "50%" });
+            ansList.Add(new Answers { ID = 2, QuestionID = 1, Text = "Indeed", Counter = 1, Result = "50%" });
+            ansList.Add(new Answers { ID = 3, QuestionID = 1, Text = "Other", Counter = 0, Result = "0%" });
+
+            _questions.Add(new Questions
+            {
+                ID = 1,
+                Text = "How did you find out about this job opportunity?",
+                Answers = ansList
+            });
+
+            _questions.Add(new Questions { ID = 2, Text = "How do you find the company’s location?" });
+
+            return _questions;
+        }
+    }
+}
